2eca2d9ff42c7c6fdf5d2dbe6f3123cd1a20267e
Jordi Escudero Subirana <jordi.escudero@gmail.com>
2019-12-04 10:58:26 +0100

    Update install-docker-amazon-aim.sh

--- aws_aim_scripts/install-docker-amazon-aim.sh
+++ aws_aim_scripts/install-docker-amazon-aim.sh
@@ -7,3 +7,11 @@ yum install -y docker
 service docker start
 
 usermod -a -G docker ec2-user
+
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+
+chmod +x /usr/local/bin/docker-compose
+
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose --version
